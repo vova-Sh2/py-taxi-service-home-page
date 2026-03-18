be7ad9759d942cbbad9a321e2c2ce18c1b058ae3
Vova <vovakucin082@gmail.com>
2026-03-18 10:41:40 +0100

    All requirements have been met

--- taxi/urls.py
+++ taxi/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from taxi.views import index
 
+
 urlpatterns = [
     path("", index, name="index"),
 ]
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+
 from pathlib import Path
 
-from django.conf.urls.static import static
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
