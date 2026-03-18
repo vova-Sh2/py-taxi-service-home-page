033c765abe101608e2878267b5364caf455c57f6
Vova <vovakucin082@gmail.com>
2026-03-18 10:28:00 +0100

    The website's homepage has been created

--- taxi/views.py
+++ taxi/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from .models import Driver, Manufacturer, Car
+
+
 def index(request: HttpRequest) -> HttpResponse:
     num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
